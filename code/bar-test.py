1214cd6af95662e4de2f5df1d276ab4a153927a9
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-01-05 19:22:12 +0200

    updates the financial plot with timestamp

--- code/bar-test.py
+++ code/bar-test.py
@@ -1,6 +1,17 @@
+#! /Users/robertpoenaru/.pyenv/shims/python
+
 import matplotlib.pyplot as plt
 import yfinance as fin
 import numpy as np
+import time
+import datetime
+
+# print(datetime.datetime.fromtimestamp(time.time()))
+
+
+date = lambda: str(datetime.datetime.fromtimestamp(time.time()))[:16]
+
+print(date())
 
 
 def ShowStocks(stock_names, stock_shares, required_shares):
@@ -18,20 +29,19 @@ def ShowStocks(stock_names, stock_shares, required_shares):
     # print(stock_shares)
 
     current_market_value = list(
-        map(lambda x: x[0]*x[1], zip(stock_shares, stock_prices)))
+        map(lambda x: x[0] * x[1], zip(stock_shares, stock_prices)))
 
     required_market_value = list(
-        map(lambda x: x[0]*x[1], zip(required_shares, stock_prices)))
+        map(lambda x: x[0] * x[1], zip(required_shares, stock_prices)))
 
     width = 0.35
     fig, ax = plt.subplots()
-    ax.bar(x_ticks-width/2, current_market_value,
-           width=width, label='current shares')
-    ax.bar(x_ticks+width/2, required_market_value,
-           width=width, label='required shares')
+    bar1 = ax.bar(x_ticks - width / 2, current_market_value,
+                  width=width, label='current shares')
+    bar2 = ax.bar(x_ticks + width / 2, required_market_value,
+                  width=width, label='required shares')
 
     fig.tight_layout()
-    # print(sum(required_market_value))
 
     # plt.annotate(
     #     f'Total \n amount={round(sum(current_market_value),3)}', [1, 250])
@@ -41,7 +51,7 @@ def ShowStocks(stock_names, stock_shares, required_shares):
              verticalalignment='center', transform=ax.transAxes, fontsize=8)
     plt.text(0.25, 0.65, f'Required \n amount={round(sum(required_market_value),3)}', horizontalalignment='center',
              verticalalignment='center', transform=ax.transAxes, fontsize=8)
-    ax.set_title('Investment portfolio')
+    ax.set_title(f'Investment portfolio \n As of: {date()}')
     ax.set_xticks(x_ticks)
     ax.set_xticklabels(stock_names)
     ax.set_ylabel('Amount $')
@@ -58,5 +68,5 @@ def ShowStocks(stock_names, stock_shares, required_shares):
 OPTIMAL_SHARE = 3.0
 names = ['AAPL', 'MSFT', 'SBUX']
 shares = [2.22, 0.69, 1.93]
-required_shares = [OPTIMAL_SHARE-x for x in shares]
+required_shares = [OPTIMAL_SHARE - x for x in shares]
 ShowStocks(names, shares, required_shares)
