bd146f72e887cd8b16090e53a45e9cd43a4087d0
Robert Poenaru <robert.poenaru@protonmail.ch>
2026-01-09 11:02:21 +0200

    add working script to compute annual dividend ammount

--- projects/dividends-builder/main.py
+++ projects/dividends-builder/main.py
@@ -1,17 +1,40 @@
 import yfinance as yf
 
+import os
+import csv
+
+PORTFOLIO_PATH = os.getenv("PORTFOLIO_PATH", None)
+assert PORTFOLIO_PATH is not None, "PORTFOLIO_PATH environment variable is not set"
+
+
+def get_portfolio():
+    with open(PORTFOLIO_PATH, 'r', encoding='utf-8') as reader:
+        data = reader.readlines()
+        portfolio = csv.DictReader(data[4:])
+        return portfolio
+
+
+def get_stock_dividend(ticker: str):
+    info = yf.Ticker(ticker).info
+    dividend_yield = info.get("dividendYield")
+    if dividend_yield is None:
+        return 0
+    dividend_yield = dividend_yield * 1e-2
+    annual_dividend_revenue = float(info.get("currentPrice")*dividend_yield)
+    return round(annual_dividend_revenue, 2)  # per share
+
 
 def main():
-    ticker = yf.Ticker("BTU")
-
-    info = ticker.info
-    dividends = ticker.dividends
-
-    # print(info)
-    print("TTM Dividend Yield:", info.get("dividendYield"), " %")
-    print("Forward Dividend Rate:", info.get("dividendRate"))
-    print("Dividend History:")
-    print(dividends.tail())
+    portfolio = get_portfolio()
+    for stock in portfolio:
+        ticker = stock['Symbol']
+        print(f'Stock: {ticker}')
+        quantity = float(stock['Quantity'])
+        annual_dividend = get_stock_dividend(ticker)
+        usd_per_share = annual_dividend
+        usd_per_stock = round(usd_per_share*quantity, 2)
+        print(
+            f'Anually {usd_per_share} $ | {usd_per_stock} $ per {quantity} shares')
 
 
 if __name__ == "__main__":
