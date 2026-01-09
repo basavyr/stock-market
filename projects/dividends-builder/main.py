4b2ef2ca85c54895472d48c09044610b9e69cb74
Robert Poenaru <robert.poenaru@protonmail.ch>
2026-01-09 09:56:36 +0200

    add main

--- projects/dividends-builder/main.py
+++ projects/dividends-builder/main.py
@@ -1,12 +1,18 @@
 import yfinance as yf
 
-ticker = yf.Ticker("BTU")
 
-info = ticker.info
-dividends = ticker.dividends
+def main():
+    ticker = yf.Ticker("BTU")
 
-# print(info)
-print("TTM Dividend Yield:", info.get("dividendYield"), " %")
-print("Forward Dividend Rate:", info.get("dividendRate"))
-print("Dividend History:")
-print(dividends.tail())
+    info = ticker.info
+    dividends = ticker.dividends
+
+    # print(info)
+    print("TTM Dividend Yield:", info.get("dividendYield"), " %")
+    print("Forward Dividend Rate:", info.get("dividendRate"))
+    print("Dividend History:")
+    print(dividends.tail())
+
+
+if __name__ == "__main__":
+    main()
